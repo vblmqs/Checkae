80a1262619f48ae2d8834c591b0e92c118a75640
Victoria Isabelle Corbolin Monte <vblmqs@gmail.com>
2025-05-21 01:53:57 -0300

    Alterações no main base

--- app/src/main/java/com/example/taskapp/MainActivity.kt
+++ app/src/main/java/com/example/taskapp/MainActivity.kt
@@ -3,33 +3,52 @@ package com.example.taskapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.taskapp.ui.theme.TaskappTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.taskapp.ui.tasklist.TaskListScreen
+import com.example.taskapp.ui.taskform.TaskFormScreen
+import com.example.taskapp.ui.subtaskform.SubtaskFormScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            TaskappTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+            MaterialTheme {
+                val navController = rememberNavController()
+                NavHost(navController = navController, startDestination = "ListaTarefas") {
+
+                    composable("ListaTarefas") {
+                        TaskListScreen(navController)
+                    }
+
+                    composable("CadastrarTarefa") {
+                        TaskFormScreen()
+                    }
+
+                    composable("CadastrarSubtarefa/{taskId}") { backStackEntry ->
+                        val taskId = backStackEntry.arguments?.getString("taskId")
+                        SubtaskFormScreen(taskId = taskId)
+                    }
+
+                    composable("EditarTarefa/{taskId}") { backStackEntry ->
+                        val taskId = backStackEntry.arguments?.getString("taskId")
+                        TaskFormScreen(taskId = taskId)
+                    }
+
+                    composable("EditarSubtarefa/{taskId}") { backStackEntry ->
+                        val taskId = backStackEntry.arguments?.getString("taskId")
+                        SubtaskFormScreen(taskId = taskId)
+                    }
                 }
             }
         }
     }
 }
 
+
+/*
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
@@ -44,4 +63,4 @@ fun GreetingPreview() {
     TaskappTheme {
         Greeting("Android")
     }
-}
+} */
